9c993118c1a9d768edc8a80c8bfaefe315fb9f12
avinirkhe39 <142663035+avinirkhe39@users.noreply.github.com>
2026-09-26 12:39:58 +0530

    Add root URL for homepage

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.home, name='root'),
     path('register/', views.register, name='register'),
     path('login/', views.user_login, name='login'),
     path('home/', views.home, name='home'),
